63f350922634280c751bd7779212a2a1c62ad81f
Serhii0588 <https://github.com/Serhii0588/genius_test.git>
2026-02-01 14:01:37 +0100

    Homework 1 completed

---
+++ js/script.js
@@ -0,0 +1,62 @@
+//***1***
+//let name = "Ivan";
+//let city = "Kyiv";
+//city = name;
+//console.log(name);
+
+//****2****
+//let name = "Olga";
+//console.log(`привіт ${1}`);
+//console.log(`привіт ${"name"}`);
+//console.log(`привіт ${name}`);
+
+//***3***
+//let a = "5";
+//let b = "13cvb";
+//let c = "12.9sxdcfgv";
+//
+//console.log(typeof Number(a));
+//console.log(typeof parseInt(b));
+//console.log(typeof parseFloat(c));
+
+//***4***
+//console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+//***5** */
+//console.log(Math.max(20, 10, 50, 40));
+
+//***6** */
+//console.log(Math.random() * (4 - 2) + 2);
+
+//**7**
+//const message = "Welcom to Bahamas";
+//console.log(message.length); //17;
+
+//**8**
+//console.log(message.toUpperCase());
+
+//** 9 **
+//let user = {};
+//user.name = "Olga";
+//user.age = 23;
+//user.city = "Kyiv";
+//console.log(user);
+//delete user.city;
+//user["like flowers"] = true;
+//console.log(user);
+
+//**10**
+
+//for (key in user) {
+//  console.log(key);
+//  console.log(user[key]);
+//}
+
+//Lesson 2
+
+//let value = "0";
+//console.log(0 <=
+
+//const message = "JavaScript is awesome!";
+//alert(message);
+//console.log(message);
